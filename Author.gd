ae7beb2c5e1938eae89afd630387470088c7152a
Linghuun <96080056+Linghuun@users.noreply.github.com>
2023-04-03 15:23:50 +0200

    can add author

--- Author.gd
+++ Author.gd
@@ -3,28 +3,35 @@ extends Node
 @onready var main = $".."
 
 
-
-@export var first_name = "test"
-@export var last_name = " tezst"
+@export var id_author: int
+@export var first_name: String
+@export var last_name: String
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-func create_author(first_name, last_name):
+func _init(id , first_name, last_name):
+	self.id_author = id
 	self.first_name = first_name
 	self.last_name = last_name
 
+func save():
+	var file = FileAccess.open("res://Author.txt", FileAccess.READ_WRITE)
+	file.seek_end()
+	file.store_string("\n{id}/{f_n}/{l_n}".format({"id": self.id_author, "f_n": self.first_name, "l_n": self.last_name}))
+	file.close()
+	
+
 func display(listing):
 	var aut = Button.new()
-	aut.text = self.first_name + " " + self.last_name
+	aut.text = first_name + " " + last_name
 	aut.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	aut.size_flags_vertical = Control.SIZE_EXPAND_FILL
 	aut.alignment = HORIZONTAL_ALIGNMENT_LEFT
--- main.gd
+++ main.gd
@@ -2,18 +2,18 @@ extends Control
 
 const Author = preload("res://Author.gd")
 const Book = preload("res://Book.gd")
+
 @onready var top_menu = $Panel/VBoxContainer/TopMenu
-@onready var listing = $Panel/VBoxContainer/DisplayContainer/ScrollContainer/Listing
 @onready var display_container = $Panel/VBoxContainer/DisplayContainer
+@onready var search_by = $Panel/VBoxContainer/TopMenu/SearchMenu/SearchBy
 
 
-@export var authors: Array = []
-
+@onready var authors = {}
+@onready var updated = false
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var A = Author.new()
-	A.create_author("f_n", "l_n")
-	
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -26,12 +26,35 @@ func create_something(text, something):
 	elt.size_flags_vertical = Control.SIZE_EXPAND_FILL
 	return elt
 	
-
+func update():
+	var file = FileAccess.open("res://Author.txt", FileAccess.READ)
+	while not file.eof_reached():
+		var line = file.get_line()
+		var data = line.split("/")
+		if len(data) == 3:
+			if int(data[0]) not in authors:
+				authors[int(data[0])] = Author.new(int(data[0]), data[1], data[2])
+	updated = true
+	file.close()
 
 func _on_search_by_item_selected(index):
+	delete_children(display_container)
+	display_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	display_container.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	var scroll_container = ScrollContainer.new()
+	scroll_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	scroll_container.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	var listing = VBoxContainer.new()
+	listing.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	listing.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	display_container.add_child(scroll_container)
+	scroll_container.add_child(listing)
 	match index:
 		0:
-			print("t")
+			if updated == false:
+				update()
+			for aut in authors:
+				authors[aut].display(listing)
 		1:
 			print("tt")
 		2:
@@ -39,7 +62,10 @@ func _on_search_by_item_selected(index):
 
 
 func _on_add_author_pressed():
-	
+	if updated == false:
+		update()
+	search_by.selected = -1
+	delete_children(display_container)
 	var hbox_first_name = HBoxContainer.new()
 	var hbox_last_name = HBoxContainer.new()
 	
@@ -60,13 +86,19 @@ func _on_add_author_pressed():
 	display_container.add_child(hbox_first_name)
 	display_container.add_child(hbox_last_name)
 	display_container.add_child(validate_button)
-	display_container.alignment = PRESET_CENTER
-	validate_button.connect("pressed" , validate)
+	
+	display_container.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+	display_container.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+	
+	validate_button.connect("pressed" , func() : validate(entry1, entry2))
 	entry1.connect("text_changed", func() : on_text_changed(entry1))
 	entry2.connect("text_changed", func() : on_text_changed(entry2))
 	
-func validate():
-	print("test")
+func validate(entry1, entry2):
+	var aut = Author.new(len(authors), entry1.text, entry2.text)
+	authors[len(authors)] = (aut)
+	aut.save()
+	delete_children(display_container)
 
 func on_text_changed(entry):
 	var text_size = entry.get_text_size()
@@ -75,4 +107,8 @@ func on_text_changed(entry):
 	print("ws", width)
 	if text_size.x > width:
 		entry.size.x = text_size
+
+func delete_children(node):
+	for child in node.get_children():
+		node.remove_child(child)
 
