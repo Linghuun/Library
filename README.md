47496ddab109f4fc892b3509dde541b1d411962b
Linghuun <96080056+Linghuun@users.noreply.github.com>
2023-04-05 21:39:35 +0200

    verification ajouté quand 2 auteurs ont même nom

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
 # Library
-
-Library (le nom est temporaire) est un projet que j'ai commencé assez récemment afin de :
-  1) Découvrir plus en profondeur le fonctionnement du Godot Engine que je trouve parfait pour facilement développer des applications de ce genre.
-  2) Créer une application qui pourrait m'aider en tant que lecteur assidu, même si je ne sais pas encore la forme que prendrais cette aide.
-
-Le code est sûrment encore flou et tout ne fonctionne pas entièrement car je suis encore entrain de l'améliorer, mais je pense qu'il est assez représentatif de mon niveau en programmation jusqu'à maintenant.
-
-L'application consiste en la création d'une bibliothèque personnelle sur laquelle on peut enregistrer des livres et leur auteur.
-On pourra peut être un jour directement y ajouter nos ebooks achetés et les y lire avec un système de notes, marque-pages, etc. Mais ce n'est pas encore le cas pour le moment.
-
-En vous souhaitant un bon visionnage,
-Adam.
+Library
--- main.gd
+++ main.gd
@@ -2,7 +2,9 @@ extends Control
 
 const Author = preload("res://Author.gd")
 const Book = preload("res://Book.gd")
-
+const verification_lab_text = """ already exist.
+Do you want
+to continue ?"""
 
 @onready var top_menu =  $Panel/VBoxContainer/TopMenu
 @onready var search_by = $Panel/VBoxContainer/TopMenu/SearchMenu/SearchBy
@@ -11,7 +13,13 @@ const Book = preload("res://Book.gd")
 @onready var listing =     $Panel/VBoxContainer/SearchMenu/ScrollContainer/Listing
 @onready var search_bar =  $Panel/VBoxContainer/SearchMenu/SearchBar
 
-@onready var add_author_menu = $Panel/VBoxContainer/AddAuthorMenu
+@onready var add_author_menu =  $Panel/VBoxContainer/AddAuthorMenu
+@onready var verification_lab = $Panel/VBoxContainer/AddAuthorMenu/VerificationBox/VerificationLab
+@onready var entry_first_name = $Panel/VBoxContainer/AddAuthorMenu/VBoxContainer/HBoxFirstName/EntryFirstName
+@onready var entry_last_name =  $Panel/VBoxContainer/AddAuthorMenu/VBoxContainer/HBoxLastName/EntryLastName
+@onready var yes_verif_aut =    $Panel/VBoxContainer/AddAuthorMenu/VerificationBox/VBoxContainer/YesVerifAut
+@onready var no_verif_aut =     $Panel/VBoxContainer/AddAuthorMenu/VerificationBox/VBoxContainer/NoVerifAut
+@onready var verification_box = $Panel/VBoxContainer/AddAuthorMenu/VerificationBox
 
 @onready var add_book_menu = $Panel/VBoxContainer/AddBookMenu
 @onready var listing2 =      $Panel/VBoxContainer/AddBookMenu/SearchMenu/ScrollContainer/Listing2
@@ -21,6 +29,8 @@ const Book = preload("res://Book.gd")
 @onready var authors = {}
 @onready var aut_updated = false
 @onready var book_updated = false
+@onready var yes_verif_aut_value = false
+@onready var no_verif_aut_value = false
 
 func update_aut():
 	var file = FileAccess.open("res://Author.txt", FileAccess.READ)
@@ -60,21 +70,38 @@ func _on_search_by_item_selected(index):
 		2:
 			print("ttt")
 
-func validate_aut(entry1, entry2):
-	if not "/" in entry1.text and not "/" in entry2.text:
-		var aut = Author.new(len(authors), entry1.text, entry2.text)
-		authors[len(authors)] = (aut)
-		aut.save()
+func _on_validate_aut_pressed():
+	if not "/" in entry_first_name.text and not "/" in entry_last_name.text:
+		var content = entry_first_name.text + " " + entry_last_name.text
+		for id in authors:
+			var author_content = authors[id].first_name + " " + authors[id].last_name
+			print(author_content)
+			if content.similarity(author_content) > 0.9:
+				verification_lab.text = author_content + "\n" + verification_lab_text
+				verification_box.visible = true
+				"""while 1:
+					if yes_verif_aut_value:
+						print("heheh")
+						yes_verif_aut_value = false
+					if no_verif_aut_value:
+						print("hohoh")
+						no_verif_aut_value = false"""
+		if verification_box.visible == false:
+			var aut = Author.new(len(authors), entry_first_name.text, entry_last_name.text)
+			authors[len(authors)] = (aut)
+			aut.save()
 
 func delete_children(node):
 	for child in node.get_children():
 		node.remove_child(child)
 
 func _on_add_author_pressed():
+	update_aut()
 	search_by.selected = -1
 	add_author_menu.visible = true
 	search_menu.visible = false
 	add_book_menu.visible = false
+	verification_box.visible = false
 	
 func _on_add_book_pressed():
 	search_bar2.clear()
@@ -104,10 +131,19 @@ func _on_search_bar_text_changed(text):
 		node = listing
 	delete_children(node)
 	if node == listing2 or index == 0: 
-		for id in authors:
-			var name_lastname = authors[id].first_name + authors[id].last_name
-			var lastname_name = authors[id].last_name + authors[id].first_name
-			if text.similarity(name_lastname) > 0.3 or text.similarity(lastname_name) > 0.3 or text in lastname_name:
-				authors[id].display(node, temp)
-		if text == "":
-			display_authors(node, temp)
+		search_bar_display_authors(text, node)
+	elif index == 1:
+		pass
+
+func search_bar_display_authors(text, node):
+	for id in authors:
+		var name_lastname = authors[id].first_name + authors[id].last_name
+		var lastname_name = authors[id].last_name + authors[id].first_name
+		if text.similarity(name_lastname) > 0.3 or text.similarity(lastname_name) > 0.3 or text in lastname_name:
+			authors[id].display(node, temp)
+	if text == "":
+		display_authors(node, temp)
+
+
+func _on_yes_verif_aut_pressed(): yes_verif_aut_value = true
+func _on_no_verif_aut_pressed(): no_verif_aut_value = true
